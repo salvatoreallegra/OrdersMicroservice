962f609f29471b367495220529dafad474f28c82
Salvatore Allegra <salallegra@gmail.com>
2025-06-02 17:56:58 -0700

    fix wrong classname, fix file path issues, restruce folders on disk properly, added dbcontext

---
+++ OrderService.Infrastructure/Persistence/OrdersDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Infrastructure.Persistence
+{
+    public class OrdersDbContext : DbContext
+    {
+        
+            public OrdersDbContext(DbContextOptions<OrdersDbContext> options) : base(options) { }
+
+            public DbSet<Order> Orders => Set<Order>();
+            public DbSet<OrderItem> OrderDetails => Set<OrderItem>();
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.ApplyConfigurationsFromAssembly(typeof(OrdersDbContext).Assembly);
+            }
+        
+    }
+
+}
